c7178489e76ed99dff4737dcc0199f2ca982356c
Yandan Wang <wangyandan418@gmail.com>
2017-02-05 18:28:58 -0500

    2level_4level_QR_for_interview_of_Chenchen

--- examples/mnist/train_lenet_mlp.sh
+++ examples/mnist/train_lenet_mlp.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
-./build/tools/caffe train --solver=examples/mnist/lenet_solver_mlp.prototxt \
- --weights=examples/mnist/mlp_500_300.caffemodel
+./build/tools/caffe train --solver=examples/mnist/lenet_solver_mlp.prototxt --weights=examples/mnist/mlp_500_300_0.983.caffemodel
 
